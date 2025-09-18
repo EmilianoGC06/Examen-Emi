c58e48833694dc39bba9cab99062ec3b2494c6f1
Emiliano Guillén <194141-a@iberoleon.edu.mx>
2025-09-18 13:46:35 -0600

    Codigos creados

---
+++ Examen-Emi/clases/areas.py
@@ -0,0 +1,12 @@
+class Areas:
+    def cuadrado(self, lado):
+        return lado * lado
+
+    def rectangulo(self, base, altura):
+        return base * altura
+
+    def triangulo(self, base, altura):
+        return (base * altura) / 2
+
+    def circulo(self, radio):
+        return 3.1416 * radio * radio
---
+++ Examen-Emi/main.py
@@ -0,0 +1,30 @@
+from areas import Areas
+
+figuras = Areas()
+
+print("1. Cuadrado")
+print("2. Rectángulo")
+print("3. Triángulo")
+print("4. Círculo")
+opcion = input("Elige una figura (1-4): ")
+
+if opcion == "1":
+    lado = float(input("Lado: "))
+    print("Área:", figuras.cuadrado(lado))
+
+elif opcion == "2":
+    base = float(input("Base: "))
+    altura = float(input("Altura: "))
+    print("Área:", figuras.rectangulo(base, altura))
+
+elif opcion == "3":
+    base = float(input("Base: "))
+    altura = float(input("Altura: "))
+    print("Área:", figuras.triangulo(base, altura))
+
+elif opcion == "4":
+    radio = float(input("Radio: "))
+    print("Área:", figuras.circulo(radio))
+
+else:
+    print("Opción inválida")
